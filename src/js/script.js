967a4695c9de5d642f33b98522df3cacbd5c7946
ArixElo <patroxgamer@gmail.com>
2024-04-01 02:21:14 +0200

    src: Add my newest device
    The rest of them will be added idk when
    
    Signed-off-by: ArixElo <patroxgamer@gmail.com>

--- src/js/script.js
+++ src/js/script.js
@@ -34,16 +34,16 @@ function toggleDropdownRom(id) {
 
 }
 
-let names = ["Xiaomi Mi 9T", "Samsung Galaxy J5 2015", "Xiaomi Redmi Note 4", "Huawei P8/P9 Lite 2017", "Xiaomi Mi 11 Lite 5G"];
-let codenames = ["davinci", "j5nlte", "mido", "prague/hi6250", "renoir"];
-let cpus = ["Qualcomm Snapdragon 730, SDM730", "Qualcomm Snapdragon 410, MSM8916", "Qualcomm Snapdragon 625, MSM8953", "HiSilicon Kirin 655, Hi6250", "Qualcomm Snapdragon 780G, SM7350-AB"];
-let gpus = ["Adreno 618", "Adreno 306", "Adreno 506", "Mali-T830MP2", "Adreno 642"];
-let memory = ["6 GB", "1.5 GB", "2 GB / 3 GB / 4 GB", "3 GB / 4 GB", "6 GB/8 GB"];
-let shippedsoft = ["Android 9, MIUI 10", "Android 5.1 Lollipop, TouchWiz", "Android 6.0 Marshmallow, MIUI 8", "Android 7.0 Nougat, EMUI 5", "Android 11, MIUI 12.5"];
-let storage = ["64 GB / 128 GB", "8 GB", "32 GB / 64 GB", "16 GB / 32 GB", "128 GB/256 GB"];
-let battery = ["4000 mAh", "2600 mAh", "4100 mAh", "3000 mAh", "4250mAh"];
-let display = ["6.39 inches AMOLED", "5.00 inches AMOLED", "5.50 inches IPS", "5.20 inches IPS", "6.55 inches 90Hz AMOLED"];
-let images = ["src/img/davinci.png", "src/img/j5nlte.png", "src/img/mido.png", "src/img/prague.png", "src/img/renoir.png"];
+let names = ["Xiaomi Mi 9T", "Samsung Galaxy J5 2015", "Xiaomi Redmi Note 4", "Huawei P8/P9 Lite 2017", "Xiaomi Mi 11 Lite 5G", "Xiaomi Redmi K60/Poco F5 Pro"];
+let codenames = ["davinci", "j5nlte", "mido", "prague/hi6250", "renoir","mondrian"];
+let cpus = ["Qualcomm Snapdragon 730, SDM730", "Qualcomm Snapdragon 410, MSM8916", "Qualcomm Snapdragon 625, MSM8953", "HiSilicon Kirin 655, Hi6250", "Qualcomm Snapdragon 780G, SM7350-AB", "Qualcomm Snapdragon 8+ Gen 1, SM8475"];
+let gpus = ["Adreno 618", "Adreno 306", "Adreno 506", "Mali-T830MP2", "Adreno 642", "Adreno 730"];
+let memory = ["6 GB", "1.5 GB", "2 GB / 3 GB / 4 GB", "3 GB / 4 GB", "6 GB/8 GB","8GB/12GB/16GB"];
+let shippedsoft = ["Android 9, MIUI 10", "Android 5.1 Lollipop, TouchWiz", "Android 6.0 Marshmallow, MIUI 8", "Android 7.0 Nougat, EMUI 5", "Android 11, MIUI 12.5","Android 13, MIUI 14.0"];
+let storage = ["64 GB / 128 GB", "8 GB", "32 GB / 64 GB", "16 GB / 32 GB", "128 GB/256 GB","128GB/256GB/512GB/1TB"];
+let battery = ["4000 mAh", "2600 mAh", "4100 mAh", "3000 mAh", "4250mAh","5160mAh/5500mAh(K60 only)"];
+let display = ["6.39 inches AMOLED", "5.00 inches AMOLED", "5.50 inches IPS", "5.20 inches IPS", "6.55 inches 90Hz AMOLED", "6.67 inches 120Hz AMOLED"];
+let images = ["src/img/davinci.png", "src/img/j5nlte.png", "src/img/mido.png", "src/img/prague.png", "src/img/renoir.png","src/img/mondrian.png"];
 
 var roms = [
     ["ShapeShiftOS", "Extended-UI", "LegionOS", "AwakenOS", "PixelExtended", "AOSP-Extended", "tequilaOS"],
